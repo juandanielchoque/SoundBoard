0a683a9f81f290cab90856b39b7da800b1432b9e
juandanielchoque <juan.choque.m@tecsup.edu.pe>
2024-10-07 17:11:00 -0500

    Initial Commit

---
+++ choqueSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  choqueSoundBoard
+//
+//  Created by juan choque on 07/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
