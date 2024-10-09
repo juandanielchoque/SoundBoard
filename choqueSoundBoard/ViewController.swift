673cd6d2743ed5f074e8255b83d25e9f2b7bdf53
juandanielchoque <juan.choque.m@tecsup.edu.pe>
2024-10-09 17:08:06 -0500

    segundo comit

--- choqueSoundBoard/ViewController.swift
+++ choqueSoundBoard/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UITableViewDelegate,
+                      UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let grabacion = grabaciones[indexPath.row]
+        do{
+            reproducirAudio = try AVAudioPlayer(data:
+                                                    grabacion.audio! as Data)
+            reproducirAudio?.play()
+        }catch{}
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let grabacion = grabaciones[indexPath.row]
+            let context = (UIApplication.shared.delegate as!
+                           AppDelegate).persistentContainer.viewContext
+            context.delete(grabacion)
+            (UIApplication.shared.delegate as!
+             AppDelegate).saveContext()
+            do{
+                grabaciones = try
+                context.fetch(Grabacion.fetchRequest())
+                tablaGrabaciones.reloadData()
+            }catch{}
+        }
     }
 
-
+    @IBOutlet weak var tablaGrabaciones: UITableView!
+    
+    var grabaciones:[Grabacion] = []
+    var reproducirAudio:AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tablaGrabaciones.dataSource = self
+        tablaGrabaciones.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as!
+                       AppDelegate).persistentContainer.viewContext
+        do {
+            grabaciones = try
+            context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        }catch{}
+    }
+    
 }
 
